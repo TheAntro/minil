34cb40a066cf19859dae71a4fc6ede659862074b
TheAntro <waltherbeach@gmail.com>
2026-03-21 20:36:52 +0200

    wip: add some theming

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      name: "Public Sans",
+      provider: fontProviders.fontsource(),
+      cssVariable: "--font-public-sans",
+    },
+    {
+      name: "Source Serif 4",
+      provider: fontProviders.google(),
+      cssVariable: "--font-source-serif",
+    },
+  ],
 });
